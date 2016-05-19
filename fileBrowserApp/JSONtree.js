3bab23b3cec45b756cca4d3de34631d12779a93a
jdelvinquier <jeremy.delvinquier@gmail.com>
2016-05-19 17:08:33 +0200

    Modif parse JSON

--- fileBrowserApp/JSONtree.js
+++ fileBrowserApp/JSONtree.js
@@ -1,11 +1,23 @@
-var rawData;
-var jsonData = [];
-var indexLevel = [1, 1, 1, 1, 1, 1, 1, 1];
+var rawData;					//Données brutes reçues par le backend
+var jsonData = [];				//Représentation de l'arbre en JSON
+var indexLevel = [1, 1, 1, 1, 1, 1, 1, 1];	//Niveau d'indexage pour la gestion des ID des nodes
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', "http://localhost:8080/kiwishare/tree", true);
-xhr.send();
-xhr.addEventListener("readystatechange", processRequest, false);
+
+/*
+ * Envoie de la requête pour la liste des fichiers
+ */
+
+function getFiles(){
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', "http://localhost:8080/kiwishare/tree", true);
+	xhr.send();
+	xhr.addEventListener("readystatechange", processRequest, false);
+}
+
+
+/*
+ * Handler de la réception de la réponse
+ */
 
 function processRequest(e) {
 	if (xhr.readyState == 4) && xhr.status == 200) {
@@ -13,33 +25,15 @@ function processRequest(e) {
 		rawData = resp.files;
 		for(var currentPath in rawData){
 			var parts = currentPath.path.split("/");
-			var nodeToAdd = fillTree(parts,null);
-			jsonData.push(nodeToAdd);
+			constructComponent(parts,jsonData);
 		}
 	}
 }
 
-function fillTree(pathParts,childNode){
-	var index = constructId(pathParts);
-	if(childNode){
-			var newParent = {id : index, title : pathParts[pathParts.length-1], nodes : []};
-			newParent.nodes.push(childNode);
-			if(pathParts.length > 1){
-				fillTree(pathParts.splice(pathParts.length-1), newParent);
-			}else{
-				return newParent;
-			}
-		}
+/*
+ * Construction de l'ID
+ */
 
-	}else{
-		var children = {id : index, title : pathParts[pathParts.length-1], nodes : []};
-		if(pathParts.length > 1){
-			fillTree(pathParts.splice(pathParts.length-1), children);
-		}else{
-			return children;
-		}
-	}
-}
 
 function constructId(pathParts){
 	var idString;
@@ -50,17 +44,75 @@ function constructId(pathParts){
 	return idString;
 }
 
-function searchExistingPaths(path,n,jsData){
-	var p = null;
-	for(var i = 0; i < jsData.length; i++){
-		data = jsData[i];
-		if(data.title == path[n]){
-			p = searchExistingPaths(path,n+1,jsData[i].nodes);
-			if(p == null){
-				return jsData[i];
+/*
+ * Cherche si une partie du path n'existe pas déjà, si oui renvoie uniquement le path non existant
+ * Exemple : "Path = Dossier1/Dossier11/Fichier" et "Tree = Dossier1" => renvoie "Path = Dossier11/Fichier"
+ */
+
+function searchExistingPaths(path,jsData,n){
+	for(var data in jsData){
+		if(data.title = path[n]){
+			var ret = searchExistingPath(path,data.nodes,n+1);
+			if(ret== null){
+				return path.splice(0,n);
 			}
 		}
 	}
 	return null;
 }
+
+/*
+ * Renvoie la dernière node déjà existante dans le tree à laquelle on doit attacher
+ * Exemple : "Path = Dossier1/Dossier11/Fichier" et "Tree = Dossier1/" => renvoie la node représentant Dossier1
+ */
+
+function getLastNodeOfPath(path,jsData,n){
+	for(var data in jsData){
+		if(data.title = path[n]){
+			var ret = getLastNodeOfPath(path,data.nodes,n+1);
+			if(ret== {}){
+				return data.nodes;
+			}
+		}
+	}
+	return {};
+}
+
+/*
+ * Ajoute l'arbre à jsonData
+ */
+
+function constructComponent(pathParts,jsonData){
+	var index = constructId(pathParts);
+	var lastNode = getLastNodeOfPath(pathParts,jsonData,0);
+	var pathToBuild = searchExistingPath(pathParts, jsonData, 0);
+	var tree = constructTreeToAdd(pathToBuild):
+	lastNode.nodes.push(tree);
+}
+
+/*
+ * Génère l'ensemble de l'arbre qu'il faut attacher à la node de jsonData
+ */
+
+function constructTreeToAdd(path){
+	var nodeList = [];
+
+	for(var i = 0; i < path.length; i++){
+		var currentNode = {id : index, title : path[path.length-1-i], nodes : []}
+		nodeList.push(currentNode);
+	}
+
+	var finalTree = nodeList[0];
+	var it = finalTree;
+	for(var i = 1; i < nodeList.length; i++){
+		it.nodes.push(nodeList[i]);
+		it = it.nodes[0];
+	}
+
+	return finalTree;
+}
+
+
+
+
 
